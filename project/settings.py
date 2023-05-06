593bef47ec73bdfc232e17b8f5e48db9761ac4fb
moamen1909 <132732466+moamen1909@users.noreply.github.com>
2023-05-06 09:19:48 +0300

    add django project structure

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-86!*s!v=sf(=ac5t!*qzwktx+ut1lqfllm)o^1zwa$rs3kr&52'
+SECRET_KEY = 'django-insecure-s+i*mv3uwqssvjq_74sa9vdy3zmn9n)9poo0+=e0s=k)&2wb%s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
